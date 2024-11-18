150f3b7c29cbd0a927d3682cb68b4b33d3e7c0e2
mnikodim <Marinenik@yandex.ru>
2024-11-18 16:37:47 +0300

    functional prog_corr1

--- module_9_1.py
+++ module_9_1.py
@@ -5,28 +5,8 @@ def apply_all_func(int_list, *functions):
     return result
 
 
-def min_num(int_list):
-    return min(int_list)
-
-
-def len_str(int_list):
-    return len(int_list)
-
-
-def sorted_list(int_list):
-    return sorted(int_list)
-
-
-def sum_num(int_list):
-    return sum(int_list)
-
-
-def max_num(int_list):
-    return max(int_list)
-
-
-print(apply_all_func([6, 20, 15, 9], min_num, max_num)) # вывод {'max': 20, 'min': 6}
-print(apply_all_func([6, 20, 15, 9], len_str, sum_num, sorted_list)) # выводим {'len': 4, 'sum': 50, 'sorted': [6, 9, 15, 20]}
+print(apply_all_func([6, 20, 15, 9], min, max)) # вывод {'max': 20, 'min': 6}
+print(apply_all_func([6, 20, 15, 9], len, sum, sorted)) # выводим {'len': 4, 'sum': 50, 'sorted': [6, 9, 15, 20]}
 
 
 
