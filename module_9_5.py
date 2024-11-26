a92d4fbc4825048954275e020ec5ff794ec4c164
mnikodim <Marinenik@yandex.ru>
2024-11-27 00:44:06 +0300

    iterator

---
+++ module_9_5.py
@@ -0,0 +1,55 @@
+class StepValueError(ValueError):
+    pass
+
+
+class Iterator:
+    def __init__(self, start, stop, step=1):
+        if step == 0:
+            raise StepValueError('Шаг не может быть равен 0')
+        self.start = start # целое число, с которого начинается итерация.
+        self.stop = stop # целое число, на котором заканчивается итерация.
+        self.step = step # шаг, с которым совершается итерация.
+        self.pointer = start # указывает на текущее число в итерации (изначально start)
+
+    def __iter__(self): # метод, сбрасывающий значение pointer на start и возвращающий сам объект итератора.
+        self.pointer = self.start
+        return self
+
+    def __next__(self): # метод, увеличивающий атрибут pointer на step.
+        if (self.step > 0 and self.pointer > self.stop) or (self.step < 0 and self.pointer < self.stop):
+            raise StopIteration
+
+        current_value = self.pointer
+        self.pointer += self.step
+        return current_value
+
+
+try:
+    iter1 = Iterator(200, 100, 0)
+    iter2 = Iterator(-5, 1)
+    iter3 = Iterator(6, 15, 2)
+    iter4 = Iterator(5, 1, -1)
+    iter5 = Iterator(10, 1)
+    for i in iter1:
+        print(i, end='')
+    print()
+    for i in iter2:
+        print(i, end='')
+    print()
+    for i in iter3:
+        print(i, end='')
+    print()
+    for i in iter4:
+        print(i, end='')
+    print()
+    for i in iter5:
+        print(i, end='')
+    print()
+except ValueError as e:
+    print(f'{e}')
+
+# Вывод на консоль:
+# Шаг указан неверно
+# -5 -4 -3 -2 -1 0 1
+# 6 8 10 12 14
+# 5 4 3 2 1
