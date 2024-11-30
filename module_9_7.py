8632de5cf3522a4e6261dcca0b8b9e1d40106991
mnikodim <Marinenik@yandex.ru>
2024-11-30 12:44:14 +0300

    decorator

--- module_9_7.py
+++ module_9_7.py
@@ -1,23 +1,24 @@
 def is_prime(func): #  распечатывает "Простое", если результат 1ой функции будет простым числом и "Составное" в противном случае
     def wrapper(*args):
         res = func(*args)
-        if res % 2:
-            print(f'Простое')
-        else:
-            print(f'Составное')
+        if res < 2:
+            return False
+        for i in range(2, int(res**0.5) + 1):
+            if res % i == 0:
+                print('Составное чиcло')
+                return res
+        print('Простое число')
         return res
     return wrapper
 
 
 @is_prime
 def sum_three(*args): # складывает 3 числа
-    sum_nums = 0
-    for nums in args:
-        sum_nums += nums
+    sum_nums = sum(args)
     return sum_nums
 
 
 result = sum_three(2, 3, 6)
-result1 = sum_three(2, 3, 7)
+result1 = sum_three(2, 3, 11)
 print(result)
 print(result1)
